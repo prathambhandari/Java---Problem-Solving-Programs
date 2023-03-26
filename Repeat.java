1c27139eac5fcedf152a96e41fb80cdb691b2b8c
prathambhandari <prathamkrishnabhandari@gmail.com>
2023-03-26 12:19:52 +0530

    Repeat problem solved

--- Repeat.java
+++ Repeat.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Repeat {
     public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the value :");
+            int n = in.nextInt();
 
+            int count = 0;
+            while (n > 0) {
+                int rem = n % 10;
+                if (rem == 2) {
+                    count++;
+                }
+                n = n / 10;
+            }
+            System.out.print("Repeated times :");
+            System.out.println(count);
+        }
     }
 
 }
