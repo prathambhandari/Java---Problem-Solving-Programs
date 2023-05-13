57805fc3607bfc05c4e2f38f275ebca21427ab01
pratham bhandari <62651866+prathambhandari@users.noreply.github.com>
2023-05-13 09:55:50 +0530

    Update and rename PrimeNumbers.java to PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,6 +1,7 @@
+
 import java.util.Scanner;
 
-public class Main {
+public class PrimeNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a number : ");
